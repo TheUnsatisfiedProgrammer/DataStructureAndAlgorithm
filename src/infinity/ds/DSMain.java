7aafe6bf1b26a4cce8e9959f7563dc8e05858908
TheUnsatisfiedProgrammer <abhishekthakur02071991@gmail.com>
2016-10-26 00:22:27 +0530

    Added ds package with main class "DSMain.java".

---
+++ src/infinity/ds/DSMain.java
@@ -0,0 +1,12 @@
+package infinity.ds;
+
+/**
+ * This is a main class.
+ */
+public class DSMain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Coming soon....!");
+    }
+}
